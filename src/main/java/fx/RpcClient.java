85e5e2e28e0ec7f11752e04e255ce473e25fd842
Yijie <jes2icazhuang@gmail.com>
2018-12-14 14:54:39 -0800

    RpcClient

--- src/main/java/fx/RpcClient.java
+++ src/main/java/fx/RpcClient.java
@@ -3,9 +3,12 @@ package fx;
 import net.corda.client.rpc.CordaRPCClient;
 import net.corda.client.rpc.CordaRPCClientConfiguration;
 import net.corda.core.contracts.StateAndRef;
+import net.corda.core.identity.CordaX500Name;
+import net.corda.core.identity.Party;
 import net.corda.core.messaging.CordaRPCOps;
 import net.corda.core.messaging.DataFeed;
 import net.corda.core.node.services.Vault;
+import net.corda.core.transactions.SignedTransaction;
 import net.corda.core.utilities.NetworkHostAndPort;
 import org.apache.activemq.artemis.api.core.ActiveMQException;
 import org.slf4j.Logger;
@@ -18,7 +21,7 @@ public class RpcClient {
     private static final Logger logger = LoggerFactory.getLogger(RpcClient.class);
 
     private static void logState(StateAndRef<TradeState> state) {
-        logger.info("{}", state.getState().getData());
+        System.out.println(String.format("{%s}", state.getState().getData()));
     }
 
     public static void main(String[] args) throws ActiveMQException, InterruptedException, ExecutionException {
@@ -31,6 +34,16 @@ public class RpcClient {
 
         // Can be amended in the com.example.Main file.
         final CordaRPCOps proxy = client.start("user1", "test").getProxy();
+        final Party otherParty = proxy.wellKnownPartyFromX500Name(CordaX500Name.parse("O=PartyB,L=New York,C=US"));
+
+        final SignedTransaction signedTx = proxy
+                .startTrackedFlowDynamic(TradeFlow.TradeInitiatorFlow.class, "23456", otherParty, "PROPOSED",
+                        "USD", 100, "CNY", 680)
+                .getReturnValue()
+                .get();
+
+        final String msg = String.format("Transaction id %s committed to ledger.\n", signedTx);
+        System.out.println(msg);
 
         // Grab all existing and future IOU states in the vault.
         final DataFeed<Vault.Page<TradeState>, Vault.Update<TradeState>> dataFeed = proxy.vaultTrack(TradeState.class);
---
+++ src/main/java/fx/TradeApi.java
@@ -0,0 +1,31 @@
+package fx;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import net.corda.core.contracts.StateAndRef;
+import net.corda.core.identity.CordaX500Name;
+import net.corda.core.identity.Party;
+import net.corda.core.messaging.CordaRPCOps;
+import net.corda.core.node.NodeInfo;
+import net.corda.core.node.services.Vault;
+import net.corda.core.node.services.vault.*;
+import net.corda.core.transactions.SignedTransaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+import static java.util.stream.Collectors.toList;
+import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
+import static javax.ws.rs.core.Response.Status.CREATED;
+
+// This API is accessible from /trade. All paths specified below are relative to it.
+@Path("trade")
+public class TradeApi {
+}
